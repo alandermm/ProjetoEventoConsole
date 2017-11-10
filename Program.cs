9cfbdd39822a34b273caa8fc48db304aecb537e3
Alander Machado <alandermm@hotmail.com>
2017-11-10 21:40:21 -0200

    Program

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using ProjetoEvento;
-using ProjetoEvento.ClassePai;
+using System.Collections.Generic;
 using ProjetoEvento.ClassePai.ClassesFilhas;
 
 namespace ProjetoEventoConsole
@@ -9,48 +8,82 @@ namespace ProjetoEventoConsole
     {
         static void Main(string[] args)
         {
+            Console.Clear();
             int opcao = 9;
             do{
+                Console.Clear();
                 Console.WriteLine("Digite a opção desejada:");
                 Console.WriteLine("1 - Shows");
                 Console.WriteLine("2 - Teatro");
                 Console.WriteLine("3 - Cinema");
                 Console.WriteLine("9 - Sair");
+                Console.Write("Digite o número da opção: ");
                 opcao = Int16.Parse(Console.ReadLine());
+
+                switch(opcao){
+                    case 1: MenuShow(); break;
+                    case 2: Teatro(); break;
+                    case 3: Cinema(); break;
+                    case 9: Environment.Exit(0); break;
+                    default: Console.WriteLine("Opção inválida!"); break;
+                }
             } while(opcao != 9 );
             
-            switch(opcao){
-                case 1: MenuShow(); break;
-                case 2: Teatro(); break;
-                case 3: Cinema(); break;
-                case 9: Environment.Exit(0); break;
-                default: Console.WriteLine("Opção inválida!"); break;
-            }
         }
 
         static void MenuShow(){
             int opcao = 9;
             do{
+                Console.Clear();
                 Console.WriteLine("Digite a opção desejada: ");
                 Console.WriteLine("1 - Cadastrar Show");
-                Console.WriteLine("2 - Pesquisar Show");
+                Console.WriteLine("2 - Pesquisar Show pelo Título");
+                Console.WriteLine("3 - Pesquisar Show pela Data");
                 Console.WriteLine("9 - Voltar");
+
+                Console.Write("Digite o número da opção: ");
+                opcao = Int16.Parse(Console.ReadLine());
+                
+                switch (opcao)
+                {
+                    case 1: { 
+                        Show show = iniciarDadosShow();
+                        bool cadastroSucesso = show.Cadastrar();
+                        if(cadastroSucesso){
+                            Console.WriteLine("Show cadastrado com sucesso!");
+                        } else {
+                            Console.WriteLine("ERRO: O show não foi cadastrado...");
+                        }
+                        break;
+                    }
+                    case 2: {
+                        Console.Write("Digite o título do show: ");
+                        string titulo = Console.ReadLine();
+                        Show show = new Show();
+                        string resultado = show.Pesquisar(titulo);
+                        Console.WriteLine(resultado);
+                        break;
+                    }
+                    case 3: {
+                        Console.Write("Digite a data do show: ");
+                        DateTime data = DateTime.Parse(Console.ReadLine());
+                        Show show = new Show();
+                        List<string> resultado = show.Pesquisar(data);
+                        Console.WriteLine(resultado);
+                        break;
+                    }
+                    case 9: 
+                            break;
+                    default: Console.WriteLine("Opção inválida!");
+                            break;
+                }
             } while(opcao != 9);
 
-            switch (opcao)
-            {
-                case 1: Show show = iniciarDadosShow();
-                        show.Cadastrar();
-                        break;
-                case 2: 
-                        break;
-                case 9: 
-                        break;
-                default: Console.WriteLine("Opção inválida!");
-                        break;
-            }
+            
         }
         static Show iniciarDadosShow(){
+            Console.Clear();
+            Console.WriteLine("Cadastro do Show:");
             Console.Write("Digite o Título do Show: ");
             string titulo = Console.ReadLine();
             Console.Write("Digite o Local do Show: ");
